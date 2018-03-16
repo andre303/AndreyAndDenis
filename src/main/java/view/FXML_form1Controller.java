fbe46cc8d4b599a33a522a896c87903b96705bfc
andre303 <157andre>
2018-03-16 21:25:35 +0200

    1234

---
+++ src/main/java/view/FXML_form1Controller.java
@@ -0,0 +1,35 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FXML_form1Controller implements Initializable {
+
+    @FXML
+    private AnchorPane primeDoc;
+
+    @FXML
+    private TextField textfield1;
+
+    @FXML
+    private Button btn1;
+
+    @FXML
+    void btn1_onClick(ActionEvent event) {
+
+    }
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+}
--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,4 +1,29 @@
 package view;
 
-public class View {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class View  extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML_form1.fxml"));
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(800);
+        primaryStage.setHeight(600);
+        primaryStage.setTitle("Main screen");
+       // primaryStage.initStyle(StageStyle.UNDECORATED);
+       // primaryStage.setResizable(false);
+        primaryStage.show();
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+
+    }
 }
